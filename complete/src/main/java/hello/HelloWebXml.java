5d4b6bd8e94c71c7e4a744b332c458494259eeff
Greg Turnquist <gturnquist@gopivotal.com>
2014-02-13 12:38:16 -0600

    Upgrade to Spring Boot 1.0.0.RC2

--- complete/src/main/java/hello/HelloWebXml.java
+++ complete/src/main/java/hello/HelloWebXml.java
@@ -1,7 +1,7 @@
 package hello;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class HelloWebXml extends SpringBootServletInitializer {
 
